ffc25a6496bda3d55cced9666065552c20cdf4ea
mac <mac@macdeMacBook-Pro-2.local>
2018-04-25 22:27:49 +0800

    add the data binding support

--- app/src/main/java/com/starwayne/databinding/MainActivity.java
+++ app/src/main/java/com/starwayne/databinding/MainActivity.java
@@ -1,13 +1,19 @@
 package com.starwayne.databinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.starwayne.databinding.bean.Person;
+import com.starwayne.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Person person = new Person("xiaoming", "36", "China");
+        binding.setPerson(person);
     }
 }
